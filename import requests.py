4420d445dcf81b59ba81bbfb491e3b635c46313e
khalilovic12 <47084915+khalilovic12@users.noreply.github.com>
2020-11-28 17:00:55 +0100

    Add files via upload

---
+++ import requests.py
@@ -0,0 +1,18 @@
+import requests
+import datetime
+import json
+import Moduleapi
+
+def main():
+
+    
+    Holidays_list = Moduleapi.get_holidays('AT')
+
+    
+
+    
+
+if __name__ == '__main__':
+    main()
+
+    
